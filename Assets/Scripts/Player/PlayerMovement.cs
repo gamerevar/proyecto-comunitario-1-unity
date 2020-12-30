61cc688a217859f9220d0c43a0e6357ae03d585b
Altiriam <allamanocostaagustin@gmail.com>
2020-12-30 19:22:17 -0300

    Agrego scrip para movimiento del Player
    Agrego script para el movimiento del player segun lo especificado en https://open.codecks.io/proyecto-comunitario-1-unity/decks/10-programación/card/14j-movimiento-del-jugador

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed;
+    private float moveInAxisX;
+    private float moveInAxisY;
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 100.0f;
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        moveInAxisX = Input.GetAxis("Vertical");
+        moveInAxisY = Input.GetAxis("Horizontal");
+
+        moveInAxisX *= Time.deltaTime;
+        moveInAxisY *= Time.deltaTime;
+
+        Vector2 speedVector = new Vector2(moveInAxisY, moveInAxisX);
+        rBody.velocity = speedVector * speed;
+
+    }
+}
