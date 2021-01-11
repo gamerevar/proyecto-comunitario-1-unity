e4d2c58a8a2d53620f422dbf06a3516b8c5c7fe2
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-01-10 21:36:51 -0300

    Agrego scrip para movimiento del Player (#14)
    * Agrego scrip para movimiento del Player
    
    Agrego script para el movimiento del player segun lo especificado en https://open.codecks.io/proyecto-comunitario-1-unity/decks/10-programación/card/14j-movimiento-del-jugador
    
    * Agrego modificaciones pedidas
    
    Agrego la normalizacion del vector, al campo speed lo hago serialisable para que se pueda editar desde el inspector y por utlimo elimino los Time ya que con un vector de norma 1 no tiene sentido.
    
    * Cambiado Input.GetAxis por Input.GetAxisRaw para obtener un movimiento mas robusto y responsivo.
    Implementado el script PlayerMovement.cs en la escena de testeo
    
    Co-authored-by: Svartskogen <amichelidebard@gmail.com>

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private float moveInAxisX;
+    private float moveInAxisY;
+    private Rigidbody2D rBody;
+
+    void Start()
+    {
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       moveInAxisX = Input.GetAxisRaw("Vertical");
+       moveInAxisY = Input.GetAxisRaw("Horizontal");
+
+       Vector2 directionVector = new Vector2(moveInAxisY, moveInAxisX);
+       rBody.velocity = directionVector.normalized * speed;
+    }
+}
