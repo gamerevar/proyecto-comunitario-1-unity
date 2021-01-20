53c7dc1b0d926ac8a3a034b0bd67e0631e6d55e8
Agustin Cundari <41393621+Cundalf@users.noreply.github.com>
2021-01-20 17:36:07 -0300

    Primera version del Enemy script. (#43)
    * Primera version del Enemy script.
    
    Issue: #42
    
    * Movi Enemy.cs a su propia carpeta
    
    Co-authored-by: Svartskogen <amichelidebard@gmail.com>

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float attackRange;
+    [SerializeField]
+    private float aggroRange;
+
+    public enum State { Idle, Moving, Attacking };
+
+    private int health;
+    private bool alive;
+    
+    private Transform player;
+    private Vector2 waypoint;
+    private State state;
+
+
+    private void Awake()
+    {
+        health = maxHealth;
+        alive = true;
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        state = State.Idle;
+        waypoint = transform.position;
+    }
+
+    private void Kill()
+    {
+        alive = false;
+    }
+
+    private void MoveTowardsWaypoint()
+    {
+        transform.Translate(waypoint.normalized * movementSpeed * Time.deltaTime);
+    }
+
+    public void Damage(int amount)
+    {
+        if (!alive) return;
+
+        health -= amount;
+
+        if(health <= 0) Kill();
+    }
+}
