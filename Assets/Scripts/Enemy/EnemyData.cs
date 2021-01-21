189ec2c23fbd9916aaafa7729cd00e062512f987
Svartskogen <amichelidebard@gmail.com>
2021-01-20 22:34:27 -0300

    EnemyData Scriptable Object

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "ScriptableObjects/EnemyData", order = 1)]
+public class EnemyData : ScriptableObject
+{
+    public int maxHealth = 100;
+    public float movementSpeed = 5f;
+    public float attackRange = 5f;
+    public float aggroRange = 5;
+}
