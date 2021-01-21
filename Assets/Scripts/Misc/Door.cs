2c898878d7ded6641589654f569ad0be4e35622a
Agustin Cundari <acundari95@gmail.com>
2021-01-21 15:24:44 -0300

    Se agrega Door script
    Se agrega dos GO, uno funcionando como activador y otro funcionando como puerta. Se utilizan los sprites disponibles y se pintan de color naranja para identificarlos y poder probarlos.

---
+++ Assets/Scripts/Misc/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door;
+    public Sprite doorOpenSprite;
+    public Sprite doorCloseSprite;
+
+    private void Start()
+    {
+        door.GetComponent<SpriteRenderer>().sprite = doorCloseSprite;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player") return;
+
+        door.GetComponent<SpriteRenderer>().sprite = doorOpenSprite;
+        door.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
