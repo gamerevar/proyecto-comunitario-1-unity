05c4d9772ecef7ca4d73b2e8970f13ebf2d84d4e
Nmac / Nikez <76722196+NNmac@users.noreply.github.com>
2021-01-22 02:53:03 -0300

    Agrego PlayerHealth.cs
    Agrego el script y lo añado al jugador en la escena prototype.

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth;
+    private int currentHealth;
+    private bool alive;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        alive = true;
+    }
+
+    public void Damage(int amount)
+    {
+        if (!alive) return;
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+            Kill();
+    }
+
+    public void Heal(int amount)
+    {
+        if (currentHealth + amount > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+    }
+
+    public bool IsAlive()
+    {
+        return alive;
+    }
+
+    public void Kill()
+    {
+        alive = false;
+    }
+}
