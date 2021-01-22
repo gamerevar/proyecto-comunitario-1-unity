97c0c3c32459b202525bb04717ec078638b5695d
Agustin Cundari <41393621+Cundalf@users.noreply.github.com>
2021-01-22 02:55:56 -0300

    Se realizan modificaciones a Enemy.cs (#49)
    * Se realizan modificaciones a Enemy.cs
    
    Issue #47
    
    * Eliminado waypoint, rediseñado MoveTowardsWaypoint a MoveTowardsPlayer
    
    Co-authored-by: Svartskogen <amichelidebard@gmail.com>

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private EnemyData data;
+
     private int maxHealth;
     private float movementSpeed;
     private float attackRange;
@@ -15,27 +18,65 @@ public class Enemy : MonoBehaviour
     private bool alive;
     
     private Transform player;
-    private Vector2 waypoint;
     private State state;
 
 
     private void Awake()
     {
+        LoadScriptableObjectData();
         health = maxHealth;
         alive = true;
-        LoadScriptableObjectData();
     }
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         state = State.Idle;
-        waypoint = transform.position;
+    }
+
+    private void Update()
+    {
+        if (!alive)
+            return;
+
+        float playerDistance = Vector3.Distance(player.position, transform.position);
+
+        switch (state)
+        {
+            case State.Idle:
+                if (playerDistance <= aggroRange)
+                    state = State.Moving;
+
+                break;
+            case State.Moving:
+                MoveTowardsPlayer();
+
+                if (playerDistance <= attackRange)
+                    Attack();
+                else if (playerDistance > aggroRange)
+                    state = State.Idle;
+
+                break;
+            case State.Attacking:
+                // TODO: Integrar con EnemyAttack
+                break;
+        }
+    }
+
+    private void Attack()
+    {
+        state = State.Attacking;
+
+        // TODO: Integrar con EnemyAttack
+        Debug.Log("Attacking");
     }
 
     private void LoadScriptableObjectData()
     {
-        //TODO
+        maxHealth = data.maxHealth;
+        movementSpeed = data.movementSpeed;
+        attackRange = data.attackRange;
+        aggroRange = data.aggroRange;
     }
 
     private void Kill()
@@ -43,9 +84,9 @@ public class Enemy : MonoBehaviour
         alive = false;
     }
 
-    private void MoveTowardsWaypoint()
+    private void MoveTowardsPlayer()
     {
-        transform.Translate(waypoint.normalized * movementSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, movementSpeed * Time.deltaTime);
     }
 
     public void Damage(int amount)
